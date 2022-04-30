b9090534bc467b2a092b03081fa790bc01798693
Zombasik <eduard61398@gmail.com>
2022-04-30 11:07:44 +0300

    add header

--- src/App.js
+++ src/App.js
@@ -4,7 +4,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     
+      <div>
+        <header>
+          <div className='head'>
+            <span><a href='#'>ТИПЫ</a></span>
+            <span><a href='#'>ВОДА</a></span>
+            <span><a href='#'>МОЛНИЯ</a></span>
+            <span><a href='#'>ЗЕМЛЯ</a></span>
+            <span><a href='#'>ВЕТЕР</a></span>
+            <span><a href='#'>ОГОНЬ</a></span>
+            <span><a href='#'>ЧАКРА</a></span>
+          </div>
+        </header>
+        <main>
+
+        </main>
+      </div>
     </div>
   );
 }
