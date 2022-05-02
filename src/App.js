5356706e30f5c27aca148755e3f34b840b4662a2
Zombasik <eduard61398@gmail.com>
2022-05-02 21:11:17 +0300

    add button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import './fontawesome-free-6.0.0-web/css/all.min.css'
+import './button/button.css'
 
 function App() {
   return (
     <div className="App">
-      <div>
+      
         <header>
           <div className='head'>
             <span><a href='#'>ТИПЫ</a></span>
@@ -16,13 +17,27 @@ function App() {
             <span><a href='#'>ЧАКРА</a></span>
           </div>
         </header>
+
         <main>
           <div className='background'>
-
+          <div className='jutsy'></div>
+            <div className='cont'>
+              <div className='cont-head'>
+                <div className='cont-head-button'>
+                  <button className='cont-head-buttonOne'>Войти</button>
+                  <input></input>
+                  <button className='cont-head-buttonTwo'>Найти</button>
+                </div>
+                <div className='cont-head-button-two'>
+                  <button><i class="fa-brands fa-vk"></i></button>
+                  <button><i class="fa-brands fa-youtube"></i></button>
+                </div>
+              </div>
+            </div>
           </div>
         </main>
       </div>
-    </div>
+
   );
 }
 
